a4a2feb0571255df85d73f43ba0c469f47d1ef47
Jormthup <jomtup12@gmail.com>
2018-06-20 10:44:07 +0700

    Start

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,39 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ShowPage } from '../show/show';
+import { Platform } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
+  
 })
 export class HomePage {
+  gaming: string = "n64";
+  gender: string = "f";
+  os: string;
+  music: string;
+  month: string;
+  year: number;
+  sn1: string = "page1";
+  isAndroid: boolean = false;
+
+  musicAlertOpts: { title: string, subTitle: string };
 
   constructor(public navCtrl: NavController) {
-
+    this.musicAlertOpts = {
+      title: '1994 Music',
+      subTitle: 'Select your favorite'
+      
+    };
   }
-
+  
+  load(){
+    this.navCtrl.push (ShowPage, {
+      val: 'anishnirmal'
+    })
+  }
+  stpSelect() {
+    console.log('STP selected');
+  }
 }
---
+++ src/pages/show/show.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the ShowPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-show',
+  templateUrl: 'show.html',
+})
+export class ShowPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    console.log(navParams.get('val'));
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad ShowPage');
+  }
+
+}
